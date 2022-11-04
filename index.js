224f245a8094f3b408f2aba1e1c58fccc388bcb5
Igor Novais <igorhnovaisdriven@gmail.com>
2022-11-04 15:17:46 -0300

    requisitos do projeto feito

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-import express from "express";
+import express, { json } from "express";
 import cors from "cors";
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+
+const signUp = [];
 
 const tweets = [
 	{
@@ -13,11 +16,42 @@ const tweets = [
 	}
 ]
 
+app.post("/sign-up", (req, res) => {
+
+	const {username, avatar} = req.body;
+
+	const user = {
+		username,
+		avatar
+	};
+
+	signUp.push(user);
+
+	res.send(signUp);
+});
+
+
+app.post("/tweets", (req,res) => {
+	const {username, tweet} = req.body;
+
+	let obj = signUp.find((item) => item.username === username);
+
+	const post = {
+		avatar: obj.avatar,
+		username,
+  		tweet
+	}
+
+	tweets.push(post);
+
+	res.send(tweets);
+});
+
 
 app.get("/tweets", (req, res) => {
     res.send(tweets);
-} )
+});
 
 app.listen(5000, () => {
     console.log("servidor ativo na porta 5000")
-})
+});
